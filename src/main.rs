5ed1543a44d8d5460d692049058036ae6e4fee79
hodongman <jhd9206@gmail.com>
2020-07-08 00:14:10 +0900

    thread variabl capture

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
 use std::thread;
-use std::time::Duration;
 
 fn main() {
 
-    let handle = thread::spawn(|| {
-        for ii in 1..10 {
-            println!("새 스레드 : {}", ii);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let value = vec![1, 2, 3];
+
+    let handle = thread::spawn( move || {
+        println!("벡터 : {:?}", value);
     });
 
     handle.join().unwrap();
-
-    for ii in 1..10 {
-        println!("주 스레드 : {}", ii);
-        thread::sleep(Duration::from_millis(1));
-    }
 }
---
+++ src/thread/thread.rs
@@ -0,0 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
+fn runThread() {
+
+    let handle = thread::spawn(|| {
+        for ii in 1..10 {
+            println!("새 스레드 : {}", ii);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    for ii in 1..10 {
+        println!("주 스레드 : {}", ii);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
