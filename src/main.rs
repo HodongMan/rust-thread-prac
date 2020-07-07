77bef9882d417af9129a616e696bce2dae03d950
hodongman <jhd9206@gmail.com>
2020-07-07 23:59:08 +0900

    thread join

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ use std::time::Duration;
 
 fn main() {
 
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for ii in 1..10 {
             println!("새 스레드 : {}", ii);
             thread::sleep(Duration::from_millis(1));
         }
     });
+
+    handle.join().unwrap();
 
     for ii in 1..10 {
         println!("주 스레드 : {}", ii);
